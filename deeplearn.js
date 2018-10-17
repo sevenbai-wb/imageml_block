e20e8be52fc67e2d1984b725ff509ea7a722a1ce
seven <sevenbai@kingkit101.com>
2018-10-17 14:58:02 +0800

    .

--- deeplearn.js
+++ deeplearn.js
@@ -6,11 +6,15 @@
   }
 }(function (scope) {
   'use strict';
-  var self = this;
-  var proto;
-  var Module = scope.Module;
+  // let self = this;
+  let proto;
+  let Module = scope.Module;
   const HOST_URL = 'https://imageml.webduino.io';
-  
+  let mobilenet;
+  let secondmodel;
+  let vid;
+  let status;
+  let labels = {};
 
   function loadJS(filePath) {
     var req = new XMLHttpRequest();
@@ -24,26 +28,51 @@
     headElement.appendChild(newScriptElement);
   }
 
+  proto.startDetect = async function () {
+    const resultTensor = tf.tidy(() => {
+      const webcamImage = tf.fromPixels(vid);
+      const batchedImage = webcamImage.expandDims(0);
+      const img = batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+      const activation = mobilenet.predict(img).flatten().expandDims(0);
+      const predictions = secondmodel.predict(activation);
+      return predictions.as1D();
+    });
+    let classTensor = resultTensor.argMax();
+    let confidenceTensor = resultTensor.max();
+    let result = {
+      class: (await classTensor.data())[0],
+      confidence: (await confidenceTensor.data())[0]
+    }
+    classTensor.dispose();
+    confidenceTensor.dispose();
+    resultTensor.dispose();
+    status.innerHTML = "辨識類別編號為：" + result.class + ",信心水準：" + parseInt(result.confidence * 1000000) / 10000.0 + " %";
+    if (typeof labels[idx] === "function") {
+      labels[idx](idx);
+    }
+    setTimeout(async ()=>{await proto.startDetect()}, 1);
+  }
+
   async function start(modelName) {
     Module.call(this);
     loadJS('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@0.12.5');
 
     // load models
     try {
-      const mobilenet = await tf.loadModel(HOST_URL + '/mobilenet/v1_0.25_224/model.json');
-      const layer = mobilenet.getLayer('conv_pw_13_relu');
-      this.mobilenet = tf.model({inputs: mobilenet.inputs, outputs: layer.output});
-      this.secondmodel = await tf.loadModel(HOST_URL + '/ml_models/' + modelName + '/model.json');
+      const _mobilenet = await tf.loadModel(HOST_URL + '/mobilenet/v1_0.25_224/model.json');
+      const layer = _mobilenet.getLayer('conv_pw_13_relu');
+      mobilenet = tf.model({inputs: _mobilenet.inputs, outputs: layer.output});
+      secondmodel = await tf.loadModel(HOST_URL + '/ml_models/' + modelName + '/model.json');
     } catch (e) {
       alert('Load model error!');
     }
 
     // create video element
-    this.vid = document.createElement('video');
-    this.vid.width = 224;
-    this.vid.height = 224;
-    this.vid.autoplay = true;
-    document.body.appendChild(this.vid);
+    vid = document.createElement('video');
+    vid.width = 224;
+    vid.height = 224;
+    vid.autoplay = true;
+    document.body.appendChild(vid);
     // start webcam
     try {
       navigator.mediaDevices.getUserMedia({
@@ -54,20 +83,20 @@
         }
       })
       .then(stream => {
-        this.vid.srcObject = stream;
-        this.vid.play();
+        vid.srcObject = stream;
+        vid.play();
       });
     } catch (e) {
       alert('WebCam is not available!');
     }
 
     // create status message
-    this.status = document.createElement('div');
-    this.status.id = 'status';
-    document.body.appendChild(this.status);
+    status = document.createElement('div');
+    status.id = 'status';
+    document.body.appendChild(status);
 
-    this.labels = {};
-    await this.startDetect();
+    labels = {};
+    await proto.startDetect();
   }
 
   function deeplearn(modelName) {
@@ -84,32 +113,7 @@
     });
 
   proto.onLabel = function (idx, callback) {
-    this.labels[idx] = callback;
-  }
-
-  proto.startDetect = async function () {
-    const resultTensor = tf.tidy(() => {
-      const webcamImage = tf.fromPixels(this.vid);
-      const batchedImage = webcamImage.expandDims(0);
-      const img = batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
-      const activation = this.mobilenet.predict(img).flatten().expandDims(0);
-      const predictions = this.secondmodel.predict(activation);
-      return predictions.as1D();
-    });
-    let classTensor = resultTensor.argMax();
-    let confidenceTensor = resultTensor.max();
-    let result = {
-      class: (await classTensor.data())[0],
-      confidence: (await confidenceTensor.data())[0]
-    }
-    classTensor.dispose();
-    confidenceTensor.dispose();
-    resultTensor.dispose();
-    this.status.innerHTML = "辨識類別編號為：" + result.class + ",信心水準：" + parseInt(result.confidence * 1000000) / 10000.0 + " %";
-    if (typeof this.labels[idx] === "function") {
-      this.labels[idx](idx);
-    }
-    setTimeout(async ()=>{await this.startDetect()}, 1);
+    labels[idx] = callback;
   }
 
   scope.module.deeplearn = deeplearn;
