64f2e12ac3bac5d93bb5e6f95137de2dd1c8b209
seven <sevenbai@kingkit101.com>
2018-10-17 14:59:52 +0800

    .

--- deeplearn.js
+++ deeplearn.js
@@ -28,31 +28,6 @@
     headElement.appendChild(newScriptElement);
   }
 
-  proto.startDetect = async function () {
-    const resultTensor = tf.tidy(() => {
-      const webcamImage = tf.fromPixels(vid);
-      const batchedImage = webcamImage.expandDims(0);
-      const img = batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
-      const activation = mobilenet.predict(img).flatten().expandDims(0);
-      const predictions = secondmodel.predict(activation);
-      return predictions.as1D();
-    });
-    let classTensor = resultTensor.argMax();
-    let confidenceTensor = resultTensor.max();
-    let result = {
-      class: (await classTensor.data())[0],
-      confidence: (await confidenceTensor.data())[0]
-    }
-    classTensor.dispose();
-    confidenceTensor.dispose();
-    resultTensor.dispose();
-    status.innerHTML = "辨識類別編號為：" + result.class + ",信心水準：" + parseInt(result.confidence * 1000000) / 10000.0 + " %";
-    if (typeof labels[idx] === "function") {
-      labels[idx](idx);
-    }
-    setTimeout(async ()=>{await proto.startDetect()}, 1);
-  }
-
   async function start(modelName) {
     Module.call(this);
     loadJS('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@0.12.5');
@@ -114,6 +89,31 @@
 
   proto.onLabel = function (idx, callback) {
     labels[idx] = callback;
+  }
+
+  proto.startDetect = async function () {
+    const resultTensor = tf.tidy(() => {
+      const webcamImage = tf.fromPixels(vid);
+      const batchedImage = webcamImage.expandDims(0);
+      const img = batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+      const activation = mobilenet.predict(img).flatten().expandDims(0);
+      const predictions = secondmodel.predict(activation);
+      return predictions.as1D();
+    });
+    let classTensor = resultTensor.argMax();
+    let confidenceTensor = resultTensor.max();
+    let result = {
+      class: (await classTensor.data())[0],
+      confidence: (await confidenceTensor.data())[0]
+    }
+    classTensor.dispose();
+    confidenceTensor.dispose();
+    resultTensor.dispose();
+    status.innerHTML = "辨識類別編號為：" + result.class + ",信心水準：" + parseInt(result.confidence * 1000000) / 10000.0 + " %";
+    if (typeof labels[idx] === "function") {
+      labels[idx](idx);
+    }
+    setTimeout(async ()=>{await proto.startDetect()}, 1);
   }
 
   scope.module.deeplearn = deeplearn;
